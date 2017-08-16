9cd85f5d3d474777518edaebad5dad9aa1b8504a
froyobin <froyobin@gmail.com>
2017-08-16 16:10:54 +1000

    modify the home page to show the polling started now

--- src/evoting/views.py
+++ src/evoting/views.py
@@ -1,9 +1,19 @@
 from django.views import generic
+from polls.models import Choice, Question
+from django.utils import timezone
 
 
-class HomePage(generic.TemplateView):
+class HomePage(generic.ListView):
     template_name = "home.html"
-
+    context_object_name = 'latest_question_list'
+    def get_queryset(self):
+        """
+        Return the last five published questions (not including those set to be
+        published in the future).
+        """
+        return Question.objects.filter(
+            pub_date__lte=timezone.now()
+        ).order_by('-pub_date')[0]
 
 class AboutPage(generic.TemplateView):
     template_name = "about.html"
